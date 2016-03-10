2ae5a823c3c10141a0e135033e5277c89a9a80f9
Susim <susim.samanta@me.com>
2016-03-10 18:15:32 +0530

    Created initial file structure

---
+++ SSCoreData/SSCoreData/SSCoreData.h
@@ -0,0 +1,19 @@
+//
+//  SSCoreData.h
+//  SSCoreData
+//
+//  Created by Susim Samanta on 10/03/16.
+//  Copyright © 2016 Susim Samanta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSCoreData.
+FOUNDATION_EXPORT double SSCoreDataVersionNumber;
+
+//! Project version string for SSCoreData.
+FOUNDATION_EXPORT const unsigned char SSCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSCoreData/PublicHeader.h>
+
+
---
+++ SSCoreData/SSCoreData/SSCoreDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  SSCoreDataManager.swift
+//  SSCoreData
+//
+//  Created by Susim Samanta on 10/03/16.
+//  Copyright © 2016 Susim Samanta. All rights reserved.
+//
+
+import UIKit
+
+class SSCoreDataManager: NSObject {
+
+}
